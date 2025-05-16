28b861c8c5d2d7653059f1e7dcc50a886a900c22
sinascode <sina@dankook.ac.kr>
2025-05-16 15:35:36 +0900

    feat: User.java

--- src/main/java/com/example/ottplatform/entity/User.java
+++ src/main/java/com/example/ottplatform/entity/User.java
@@ -1,5 +1,24 @@
 package com.example.ottplatform.entity;
 
-public class User {
+import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userno;
+
+    @Column(nullable = false, unique = true)
+    private String uid;
+
+    @Column(nullable = false)
+    private String upw;
+
+    @Column(nullable = false)
+    private String role; // "user" 또는 "admin"
 }
