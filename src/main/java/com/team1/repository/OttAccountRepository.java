f79cbc5e147e9f4d52a022b9caa52a9c60d03275
sinascode <sina@dankook.ac.kr>
2025-05-19 14:57:13 +0900

    feat: OttAccountRepository.java

--- src/main/java/com/team1/repository/OttAccountRepository.java
+++ src/main/java/com/team1/repository/OttAccountRepository.java
@@ -1,5 +1,7 @@
 package com.team1.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.team1.entity.OttAccount;
 
 @Repository
 public interface OttAccountRepository extends JpaRepository<OttAccount, Long> {
-
+	 // 선택한 플랫폼에 해당하는 계정 반환
+    Optional<OttAccount> findFirstByOttNo_OttNo(Long ottNo);
 }
