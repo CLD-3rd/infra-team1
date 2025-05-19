47f0f56a8d3fac2151f6942b2730964ab1a3aaf6
wonshee <shee2.won@gmail.com>
2025-05-19 11:23:17 +0900

    feat : user login

--- src/main/java/com/team1/service/UserAccountService.java
+++ src/main/java/com/team1/service/UserAccountService.java
@@ -17,4 +17,8 @@ public class UserAccountService {
 	
 	private final UserAccountRepository userAccountRepository;
 
+    public UserAccount authenticate(String uid, String upw) {
+        return userAccountRepository.findByUidAndUpw(uid, upw);
+    }
+
 }
