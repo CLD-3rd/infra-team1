23fa73dd0631a75ac2f74871dae666de433cfac8
kimrma370 <kimrma370@gmail.com>
2025-05-19 15:09:39 +0900

    feat: add API to retrieve all shared OTT accounts

--- src/main/java/com/team1/controller/OttAccountController.java
+++ src/main/java/com/team1/controller/OttAccountController.java
@@ -1,8 +1,13 @@
 package com.team1.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.team1.dto.OttAccountDto;
 import com.team1.service.OttAccountService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,5 +18,11 @@ import lombok.RequiredArgsConstructor;
 public class OttAccountController {
 	
 	private final OttAccountService ottAccountService;
-
+	
+	@GetMapping("/ott-acc-list")
+	@ResponseBody
+	public List<OttAccountDto> getAllAccounts() {
+		return ottAccountService.getAllAccounts();
+	}
+	
 }
--- src/main/java/com/team1/service/OttAccountService.java
+++ src/main/java/com/team1/service/OttAccountService.java
@@ -1,8 +1,13 @@
 package com.team1.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.team1.dto.OttAccountDto;
+import com.team1.entity.OttAccount;
 import com.team1.repository.OttAccountRepository;
 import com.team1.repository.UserAccountRepository;
 
@@ -14,5 +19,21 @@ import lombok.RequiredArgsConstructor;
 public class OttAccountService {
 	
 	private final OttAccountRepository ottAccountRepository;
+
+	public List<OttAccountDto> getAllAccounts() {
+		return ottAccountRepository.findAll()
+											.stream()
+											.map(this::toDto)
+											.collect(Collectors.toList());
+	}
+	
+	private OttAccountDto toDto(OttAccount entity) {
+		return new OttAccountDto(
+				entity.getOaccNo().intValue(),
+				entity.getOttNo().getOttNo().intValue(),
+				entity.getEmail(),
+				entity.getPassword()
+				);
+	}
 
 }
